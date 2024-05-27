ecc07d18f67f2924fc744350f1ec783232848a4a
Dmytro <dashkin.dima@rl.odessa.ua>
2024-05-27 13:48:19 +0300

    Fixed user model

--- PulseFlow/settings.py
+++ PulseFlow/settings.py
@@ -108,8 +108,8 @@ DATABASES = {
 }
 
 AUTH_USER_MODEL = "pulse.User"
-# Password validation
-# https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
+
+
 
 AUTH_PASSWORD_VALIDATORS = [
     {
